43fcef0789e6a69ec78cd89e8ef4b25f86bf0484
raybellwaves <rjbell1987@gmail.com>
2018-06-21 12:56:08 -0400

    drop M from obs file

--- Python/create_obs_anomalies/ERA-Interim/createobsanom_ts_template.py
+++ Python/create_obs_anomalies/ERA-Interim/createobsanom_ts_template.py
@@ -97,7 +97,7 @@ if create_anom == 1:
     # Put observationis into model format
     # Open model anomaly file
     _da = xr.open_dataarray(anomDir+anomfname)
-    obs = _da.isel(M=0).copy()
+    obs = _da.isel(M=0).drop('M').copy()
     for i, _L in enumerate(_da.L):
         _Sindex = _da.S + pd.Timedelta(str(i)+' days')
         obs[:, i] = da.sel(time=_Sindex)
