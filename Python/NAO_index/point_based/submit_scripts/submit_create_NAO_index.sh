74332b2e4134398387320b36de60d11711ef9bd2
raybellwaves <rjbell1987@gmail.com>
2018-06-21 20:33:21 -0400

    change wall clock time

--- Python/NAO_index/point_based/submit_scripts/submit_create_NAO_index.sh
+++ Python/NAO_index/point_based/submit_scripts/submit_create_NAO_index.sh
@@ -2,7 +2,7 @@
 #BSUB -J create_NAO_index.py
 #BSUB -o logs/create_NAO_index.out
 #BSUB -e logs/create_NAO_index.err
-#BSUB -W 24:00
+#BSUB -W 1:00
 #BSUB -q general
 #BSUB -n 1
 #
--- Python/create_anomalies/submit_scripts/submit_create_ts_anom.sh
+++ Python/create_anomalies/submit_scripts/submit_create_ts_anom.sh
@@ -2,7 +2,7 @@
 #BSUB -J create_ts_anomalies.py
 #BSUB -o logs/create_ts_anomalies.out
 #BSUB -e logs/create_ts_anomalies.err
-#BSUB -W 24:00
+#BSUB -W 1:00
 #BSUB -q general
 #BSUB -n 1
 #
