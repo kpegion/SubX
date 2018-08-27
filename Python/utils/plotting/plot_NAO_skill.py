daa5302e95f4555590bc7943cc2b1c695552491e
Ray Bell <rjbell1987@gmail.com>
2018-08-27 11:46:41 -0400

    Update plot_NAO_skill.py

--- Python/utils/plotting/plot_NAO_skill.py
+++ Python/utils/plotting/plot_NAO_skill.py
@@ -24,21 +24,23 @@ fig.set_size_inches(23.5, 8.5)
 # Correlation plot
 ax1.plot(np.arange(1, 46), np.full((45), 0.5), 'gray', linewidth=0.5)
 ax1.set_yticks(np.arange(0, 1.1, 0.1))
-ax1.set_title('Anomaly correlation')
+ax1.set_title('Anomaly correlation', fontsize=16)
 ax1.set_xticks(np.arange(5, 50, 5))
-ax1.set_xlabel('Lead Time (Days)')
+ax1.set_xlabel('Lead Time (Days)', fontsize=16)
+ax1.tick_params(axis='both', which='major', labelsize=16)
+ax1.tick_params(axis='both', which='minor', labelsize=16)
 
 # RMSE plot
 ax2.plot(np.arange(1, 46), np.full((45), 1.4), 'gray', linewidth=0.5)
 ax2.set_yticks(np.arange(0, 2.2, 0.2))
-ax2.set_title('RMSE')
+ax2.set_title('RMSE', fontsize=16)
+ax2.tick_params(axis='both', which='major', labelsize=16)
+ax2.tick_params(axis='both', which='minor', labelsize=16)
 
-plt.suptitle('SubX NAO Skill [Dec-Feb ICs]')
-
+plt.suptitle('SubX NAO Skill [Dec-Feb ICs]', fontsize=16)
 
 # Loop over models
-models = ['30LCESM1', '46LCESM1', 'CCSM4', 'FIMr1p1', 'GEFS', 'GEM',
-          'GEOS_V2p1', 'NESM', 'MME']
+models = ['CCSM4', 'FIMr1p1', 'GEFS', 'GEM', 'GEOS_V2p1', 'NESM']
 for i, model in enumerate(models):
     # Read in model data
     da = xr.open_dataarray(ddir+model+fdir+fname)
@@ -47,13 +49,37 @@ for i, model in enumerate(models):
     da = xr.open_dataarray(obsdir+obsfname+model+'.nc')
     obs = da.loc[da['S.season']=='DJF']
     
-    r = xs.pearson_r(obs, fct, 'S')
-    _x = np.arange(1, len(da.L)+1)
-    ax1.plot(_x, r.values, label=model)
+    # Rename some models
+    _model = model
+    if _model == 'CCSM4':
+        _line_color = 'black'       
+    if _model == 'FIMr1p1':
+        _model = 'FIM'
+        _line_color = 'red'
+    if _model == 'GEFS':
+        _line_color = 'green'
+    if _model == 'GEM':
+        _line_color = 'blue'
+    if _model == 'GEM':
+        _line_color = 'blue'
+    if _model == 'GEOS_V2p1':
+        _model = 'GEOS'
+        _line_color = 'purple'
+    if _model == 'NESM':
+        _line_color = 'orange'        
     
+    r = xs.pearson_r(obs, fct, 'S')
+    _r = r.values
     rmse = xs.rmse(obs, fct, 'S')
-    ax2.plot(_x, rmse.values)
-ax1.legend(loc="upper right")
+    _rmse = rmse.values
+    
+    _x = np.arange(1, len(da.L)+1)
+        
+    ax1.plot(_x, _r, label=_model, linewidth=2, color=_line_color)
+    
+    ax2.plot(_x, _rmse, linewidth=2, color=_line_color)
+    
+ax1.legend(loc="upper right", fontsize=16)
 
 plt.savefig(fsavename + '.png', bbox_inches = 'tight')
 plt.savefig(fsavename + '.eps', bbox_inches = 'tight', format = 'eps')
